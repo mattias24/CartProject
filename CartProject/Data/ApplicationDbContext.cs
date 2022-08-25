7ee9bf6d9f6ff3b015cba607e41e27cf38fa3e01
mattiasandersson0224@gmail.com <mattiasandersson0224@gmail.com>
2022-08-25 16:06:32 +0200

    Add project files.

---
+++ CartProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CartProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CartProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        public DbSet<Products> Products { get; set; }
+            public DbSet<Category> Category { get; set; }
+   
+
+    }
+}
---
+++ CartProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CartProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
